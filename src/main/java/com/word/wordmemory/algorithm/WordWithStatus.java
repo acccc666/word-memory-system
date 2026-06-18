38e94a1843411fa78eb209c7749ac535e7c8c1bf
Dream-Alan <805292940@qq.com>
2026-06-18 18:38:39 +0800

    feat: 加入基于艾宾浩斯记忆曲线的单词出题算法的基本框架

---
+++ src/main/java/com/word/wordmemory/algorithm/WordWithStatus.java
@@ -0,0 +1,18 @@
+package com.word.wordmemory.algorithm;
+
+import com.word.wordmemory.entity.Word;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class WordWithStatus {
+    private Word word;
+    private Integer status; // 0:未记, 1:模糊, 2:已记
+
+    // 新增：记录该用户上一次背这个词的时间
+    private LocalDateTime lastReviewTime;
+
+    // 新增：不存数据库，只在算法执行时作为排序依据的“战斗力分数”
+    private Double priorityScore;
+}
