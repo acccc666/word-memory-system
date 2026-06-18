673ad52bb0a46009235f23e725e898f4a7915081
Dream-Alan <805292940@qq.com>
2026-06-18 20:13:38 +0800

    feat: 完善基于艾宾浩斯记忆曲线的单词出题算法

--- src/main/java/com/word/wordmemory/mapper/WordMapper.java
+++ src/main/java/com/word/wordmemory/mapper/WordMapper.java
@@ -2,8 +2,22 @@ package com.word.wordmemory.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.word.wordmemory.entity.Word;
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import java.util.List;
 
-@Mapper
-public interface WordMapper extends BaseMapper <Word>{
+// 注意：这里的 WordWithStatus 是我们在之前那步定义的 DTO 类
+import com.word.wordmemory.algorithm.WordWithStatus;
+
+public interface WordMapper extends BaseMapper<Word> {
+
+    /**
+     * 联表查询：获取用户在某本单词书下的所有单词及其记忆状态
+     */
+    @Select("SELECT w.*, s.status, s.last_review_time " +
+            "FROM word w " +
+            "LEFT JOIN user_word_status s " +
+            "  ON w.id = s.word_id AND s.user_id = #{userId} " +
+            "WHERE w.book_id = #{bookId}")
+    List<WordWithStatus> selectWordsWithStatus(@Param("userId") Long userId, @Param("bookId") Long bookId);
 }
